a95f09bfd7d59da086520349d2b712e84c6f70f5
cali346 <77466286+praisepelumi@users.noreply.github.com>
2021-10-05 20:55:27 -0400

    Add beta version of site

---
+++ README.md
@@ -0,0 +1,50 @@
+# simple-weather-app
+
+## Table of contents
+
+  - [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot of live site](#screenshots-of-live-site)
+  - [Links to solution](#links-to-solution)
+  - [What I learned](#what-i-learned)
+  - [Built with](#built-with)
+  - [Author](#author)
+
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- Type in any city and receive weather information, date, and time about that city. 
+
+### Screenshots-of-live-site
+Here is a screenshot of my solution for the site. 
+
+This is the mobile site
+![Mobile Site](/images/3CnMobile.jpeg?raw=true "Mobile Site")
+
+### Links-to-solution
+
+- Solution URL: (https://github.com/praisepelumi/simple-weather-app.git)
+- Live Site URL: (https://praisepelumi.github.io/simple-weather-app)
+
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- Openweather.org API
+- Mobile-first workflow
+
+### What I learned
+
+- I learnt how to utilize data from third party APIs and apply the data on my site
+
+## Author
+
+- Website - [Praise Emmanuel](https://www.praiseemmanuel.com/)
+- Github - [Praise Emmanuel](https://github.com/praisepelumi)
+- Linkedin - [Praise Emmanuel](https://www.linkedin.com/in/praise-emmanuel-37378a1a8/)
--- app.js
+++ app.js
@@ -2,20 +2,59 @@ const form = document.querySelector("#weatherForm");
 const button = document.querySelector("button");
 let weatherName = document.querySelector(".weather-Name")
 let image = document.querySelector("img");
-let display = document.querySelector(".display")
+let temperatureName = document.querySelector(".temp-name")
+let temperatureNumber = document.querySelector(".temp-num");
+let tempDegree = document.querySelector(".temp")
+let showCity = document.querySelector(".curCity")
+let showTime = document.querySelector(".curTime")
 
-const apiKey = `f4ba1c5c45874efb70c3f391ae05af62`;
 
 form.addEventListener('submit', async function (e) {
     e.preventDefault();
     const cityName = form.elements.query.value
     // const config = { prarams: { q: cityName,  appid: apiKey}}
-    const res = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=f4ba1c5c45874efb70c3f391ae05af62
-    `)
+    const res = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=f4ba1c5c45874efb70c3f391ae05af62&units=imperial`)
 
-   weatherName.innerText = res.data.weather[0].main
-   image.src =  `http://openweathermap.org/img/w/${res.data.weather[0].icon}.png`
-   
-   
+    let temperatureName = res.data.weather[0].main
+    displayTempName(temperatureName);
+
+    let imgSrc = `http://openweathermap.org/img/w/${res.data.weather[0].icon}.png`
+    displayIcon(imgSrc)
+
+    let degree = res.data.main.temp
+    console.log(degree)
+    displayTempNumber(degree);
+
+    let city = res.data.name
+    displayCity(city);
+
+    let cityTimeZone = res.data.timezone
+    generateTime(cityTimeZone);
 
 })
+
+function generateTime(timeZone) {
+    let d = new Date()
+    let localTime = d.getTime()
+    let localOffset = d.getTimezoneOffset() * 60000
+    let utc = localTime + localOffset
+    var cityTime = utc + (1000 * timeZone)
+    let newDate= new Date(cityTime)
+    showTime.innerText = newDate;
+}
+
+function displayIcon (source) {
+    image.src = source;
+}
+
+function displayTempName (tempName) {
+    temperatureName.innerText = tempName;
+}
+
+function displayTempNumber (tempNum) {
+    temperatureNumber.innerText = `${tempNum}F`;
+}
+
+function  displayCity (city) {
+    showCity.innerText = city
+}
